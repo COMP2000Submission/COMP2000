6c591966da218442d4bac8cbc79389a0a44232dd
Hiang <hiangloong.tan@students.mq.edu.au>
2026-09-01 10:24:18 +1000

    Added elements and their characteristics

--- Elements.java
+++ Elements.java
@@ -1,7 +1,38 @@
 public class Elements {
-    boolean CanFall;
+    boolean moveable;
+    boolean canBurn;
+    String color;
+    boolean ReverseGravity;
 
-    public Elements(boolean CanFall) {
-        this.CanFall = CanFall;
+    public Elements(boolean moveable, boolean canBurn, String color, boolean ReverseGravity) {
+        this.moveable = moveable;
+        this.canBurn = false;
+        this.color = color;
+        this.ReverseGravity = false;
+    }
+
+    class Sand extends Elements {
+        public Sand() {
+            super(true, false, "yellow", false);
+        }
+    }
+
+    class Water extends Elements {
+        public Water() {
+            super(true, false, "blue", false);
+        }
+    }
+
+    class Fire extends Elements {
+        public Fire() {
+            super(false, true, "red", false);
+        }
+    }
+    class Gas extends Elements {
+        public Gas() {
+            super(false, false, "clear", false);
+        }
     }
 }
+
+

3ea2dbaeb944363a874d4fc456506a4d7ea11834
MarcussShidiakk <marcus.shidiak@students.mq.edu.au>
2026-09-05 12:50:21 +1000

    Marcus - Restructure, Added Main and SimulationPanel.  Reformatted every file.
    After our discussion in the team meeting, I added some new files and moved around some code. Nothing much added though.

--- Sandbox.java
+++ Sandbox.java
@@ -1,6 +1,6 @@
 public class Sandbox {
 
-    static int WIDTH = 800; // Width and Height are changeable, not sure if this is too big.
+    static int WIDTH = 800;
     static int HEIGHT = 600;
     static int CELL_SIZE = 4;
 
@@ -9,7 +9,7 @@ public class Sandbox {
 
     Elements[][] grid;
 
-public Sandbox() {
+    public Sandbox() {
         grid = new Elements[ROWS][COLS];
         grid[20][40] = new Sand();
     }
@@ -18,10 +18,10 @@ public Sandbox() {
         for (int row = ROWS - 2; row >= 0; row--) {
             for (int col = 0; col < COLS; col++) {
 
-                // Check if  cell contains an element
+                // Check if cell contains an element
                 if (grid[row][col] != null) {
 
-                    // Check if  cell below is empty
+                    // Check if cell below is empty
                     if (grid[row + 1][col] == null) {
 
                         // Gravity
@@ -32,10 +32,4 @@ public Sandbox() {
             }
         }
     }
-
-    public static void main(String[] args) {
-        Sandbox sandbox = new Sandbox();
-        UI frame = new UI(WIDTH, HEIGHT, sandbox);
-    }
 }
-
--- UI.java
+++ UI.java
@@ -1,5 +1,4 @@
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 
 public class UI extends JFrame {
@@ -9,24 +8,24 @@ public class UI extends JFrame {
     public UI(int width, int height, Sandbox sandbox) {
         this.sandbox = sandbox;
 
-        //Frame Creation
+        // Frame Creation
         setTitle("COMP2000 Project");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Main Layout
+        // Main Layout
         setLayout(new BorderLayout());
         JPanel mainPanel = new JPanel(new BorderLayout());
         mainPanel.setBackground(Color.GRAY);
 
         add(mainPanel, BorderLayout.CENTER);
 
-        //Left Area
+        // Left Area
         JPanel leftPanel = new JPanel(new BorderLayout());
         leftPanel.setBackground(Color.GRAY);
 
         mainPanel.add(leftPanel, BorderLayout.CENTER);
 
-        //Simulation Area - might need to change this later.                                                                     !!!!!!
+        // Simulation Area - might need to change this later. !!!!!!
         JPanel simWrapper = new JPanel(new FlowLayout(
                 FlowLayout.LEFT,
                 0,
@@ -34,7 +33,7 @@ public class UI extends JFrame {
 
         simWrapper.setBackground(Color.GRAY);
 
-        JPanel simPanel = new SimPanel();
+        JPanel simPanel = new SimulationPanel(sandbox);
 
         simPanel.setPreferredSize(new Dimension(
                 Sandbox.WIDTH,
@@ -46,14 +45,14 @@ public class UI extends JFrame {
 
         leftPanel.add(simWrapper, BorderLayout.NORTH);
 
-        //Side Bar
+        // Side Bar
         JPanel sidePanel = new JPanel();
         sidePanel.setPreferredSize(new Dimension(150, 0));
         sidePanel.setBackground(Color.GRAY);
 
         mainPanel.add(sidePanel, BorderLayout.EAST);
 
-        //Bottom Bar
+        // Bottom Bar
         JPanel bottomPanel = new JPanel();
         bottomPanel.setBackground(Color.GRAY);
 
@@ -65,65 +64,5 @@ public class UI extends JFrame {
 
         setLocationRelativeTo(null);
         setVisible(true);
-
-        //Simulation Timer
-
-        Timer timer = new Timer(16, e -> {
-            sandbox.step();
-            simPanel.repaint();
-        });
-        timer.start();
-    }
-
-    // draws or creates the sandbox and handles clicks within the simultaed area
-    class SimPanel extends JPanel {
-        SimPanel() {
-            // Converting mous to pixels and allows us to spawn our material there
-            MouseAdapter mouse = new MouseAdapter() {
-                void place(MouseEvent e) {
-                    int col = e.getX() / Sandbox.CELL_SIZE;
-                    int row = e.getY() / Sandbox.CELL_SIZE;
-                    // Collision detection to ensure that we cant click outside of the simulated enviroment/grid
-                    if (row >= 0 && row < Sandbox.ROWS && col >= 0 && col < Sandbox.COLS) {
-                        sandbox.grid[row][col] = new Sand();
-                        repaint();
-                    }
-                }
-
-                // mous events for click and clicks+drag
-                @Override
-                public void mousePressed(MouseEvent e) {
-                    place(e);
-                }
-
-                @Override
-                public void mouseDragged(MouseEvent e) {
-                    place(e);
-                }
-            };
-            addMouseListener(mouse);
-            addMouseMotionListener(mouse);
-        }
-
-        // called whenever the panel needs to redraw
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            
-            int s = Sandbox.CELL_SIZE;
-            
-            for (int row = 0; row < Sandbox.ROWS; row++) {
-                for (int col = 0; col < Sandbox.COLS; col++) {
-                    
-                    Elements element = sandbox.grid[row][col];
-                    
-                    if (element != null) {
-                        
-                        g.setColor(element.getColor());
-                        
-                        g.fillRect(col * s, row * s, s, s);
-                    }
-                }
-            }
-        }
     }
 }
